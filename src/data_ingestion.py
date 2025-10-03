e571eb374e94d74a7d092e83b59a773f7f86ec54
Nithira Jayarathne <hacnjayarathne@gmail.com>
2025-10-03 20:32:25 +0530

    Initialized pyspark

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import logging
+from typing import Optional
 from abc import ABC, abstractmethod
+from pyspark.sql import DataFrame, SparkSession
+from spark_session import get_
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 class DataIngestor(ABC):
+    def __init__(self, spark: Optional[SparkSession] = None):
     @abstractmethod
     def ingest(self, file_path_or_link: str) ->pd.DataFrame:
         pass
